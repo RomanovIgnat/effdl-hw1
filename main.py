4fb2abcf6524e4162f616832ab2673239c6d0970
RomanovIgnat <romanov.ignat.p@gmail.com>
2023-02-18 19:07:43 +0300

    try_wandb

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ from modeling.diffusion import DiffusionModel
 from modeling.training import generate_samples, train_epoch
 from modeling.unet import UnetModel
 
+import wandb
+import yaml
 
-def main(device: str, num_epochs: int = 100):
+
+def main(device: str, config: dict):
     ddpm = DiffusionModel(
-        eps_model=UnetModel(3, 3, hidden_size=128),
-        betas=(1e-4, 0.02),
-        num_timesteps=1000,
+        eps_model=UnetModel(3, 3, hidden_size=config['eps_model_hidden']),
+        betas=(config['beta_1'], config['beta_2']),
+        num_timesteps=config['num_timestamps'],
     )
     ddpm.to(device)
 
@@ -27,14 +30,28 @@ def main(device: str, num_epochs: int = 100):
         transform=train_transforms,
     )
 
-    dataloader = DataLoader(dataset, batch_size=128, num_workers=4, shuffle=True)
-    optim = torch.optim.Adam(ddpm.parameters(), lr=1e-5)
+    dataloader = DataLoader(dataset, batch_size=config['batch_size'], num_workers=4, shuffle=True)
+    optim = torch.optim.Adam(ddpm.parameters(), lr=config['lr'])
 
-    for i in range(num_epochs):
-        train_epoch(ddpm, dataloader, optim, device)
+    wandb.init(
+        project="effdl-hw1",
+        config=config
+    )
+
+    for i in range(config['num_epochs']):
+        loss = train_epoch(ddpm, dataloader, optim, device)
         generate_samples(ddpm, device, f"samples/{i:02d}.png")
+        wandb.log(
+            {
+                'train loss': loss,
+                'lr': optim.param_groups[0]['lr'],
+                'samples': wandb.Image(f"samples/{i:02d}.png")
+            }
+        )
 
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    main(device=device)
+    with open('hyperparam.yaml') as config_file:
+        config = yaml.safe_load(config_file)
+        main(device=device, config=config)
